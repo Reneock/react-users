31ed64e8021b1889c4407cd3b14d71f0bd12b4c0
Irene Ockrah <ostrene33@gmail.com>
2022-09-17 05:55:26 -0700

    creating and adding data in redux

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,10 +1,5 @@
-import {v4 as uuid} from "uuid";
-
 let initialState = {
-  users: [
-  {name:"Frimp Fos", email: "frimp@gmail.com", gen: 15, id: uuid(),},
-  {name:"Enoch Jon", email: "enoch@gmail.com", gen: 18, id: uuid(),},
-],
+  users: [],
 };
 
 
