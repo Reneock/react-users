7ef1cf1486f4e47366cdc316c29984fdcc442535
Irene Ockrah <ostrene33@gmail.com>
2022-10-06 23:30:34 -0700

    updating app.js

--- src/App.js
+++ src/App.js
@@ -6,31 +6,6 @@ import UsersForm from './components/UsersForm';
 import './App.css';
 
 class App extends Component {
- 
- constructor(props) {
-  super();
-  this.state = { users: [] }
-}
-
- addNewUser=(user)=>{
-  user.id = Math.random().toString()
-  this.setState({
-    users: [...this.state.users, user]
-  })
- }
-
- deleteUser = (id) => {
-  let undeletedUsers = this.state.users.filter ((user) => user.id !== id);
-  this.setState({
-    users: undeletedUsers
-  })
- }
-
- editForm = (id, updatedUser) => {
-  this.setState({
-    users: this.state.users.map(user => user.id === id ? updatedUser : user)
-  })
- }
 
  render(){ 
     return (
@@ -38,10 +13,10 @@ class App extends Component {
         <Container fluid style={{marginTop: "2rem"}}>
           <Row>
             <Col md="4">
-             <UsersForm addUser ={this.addNewUser}/>
+             <UsersForm />
             </Col>
             <Col>
-             <Users usersData={this.state.users} deleteUser={this.deleteUser} editForm={this.editForm}/>
+             <Users/>
             </Col>
           </Row>
         </Container>
